a7be4993804246520756c32e1ad5988a961858ac
Jairo Portillo <portillo.jairojr@gmail.com>
2025-08-25 20:52:51 -0500

    Removed old Song.py

--- Song.py
+++
@@ -1,8 +0,0 @@
-class Song:
-    def __init__(self, title, url, id):
-        self.title = title
-        self.url = url
-        self.id = id
-
-    def __str__(self):
-        return f'{self.title} {self.url} {self.id}'
