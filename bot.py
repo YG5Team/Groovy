8ba35f4a3c0d4d776c0e43ceb88861993ee3dc3a
Frank Martinez <frankm-7@tamu.edu>
2025-09-28 16:11:10 -0500

    changed bot init command

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ log = logging.getLogger("groovy")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
